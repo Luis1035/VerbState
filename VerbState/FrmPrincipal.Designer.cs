abd5aa0e2b0d35c9617ca7cea79f3d91c5833fce
Luis1035 <alcala_luis@hotmail.com>
2022-07-14 12:06:39 -0500

    inicio de verbs state

---
+++ VerbState/FrmPrincipal.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace VerbState
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.verbDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.traduccionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.verbStateBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.txtFiltro = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verbStateBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.AutoGenerateColumns = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.verbDataGridViewTextBoxColumn,
+            this.stateDataGridViewTextBoxColumn,
+            this.traduccionDataGridViewTextBoxColumn});
+            this.dgvData.DataSource = this.verbStateBindingSource;
+            this.dgvData.Location = new System.Drawing.Point(1, 32);
+            this.dgvData.MultiSelect = false;
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvData.Size = new System.Drawing.Size(520, 271);
+            this.dgvData.TabIndex = 0;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // verbDataGridViewTextBoxColumn
+            // 
+            this.verbDataGridViewTextBoxColumn.DataPropertyName = "Verb";
+            this.verbDataGridViewTextBoxColumn.HeaderText = "Verb";
+            this.verbDataGridViewTextBoxColumn.Name = "verbDataGridViewTextBoxColumn";
+            this.verbDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // stateDataGridViewTextBoxColumn
+            // 
+            this.stateDataGridViewTextBoxColumn.DataPropertyName = "State";
+            this.stateDataGridViewTextBoxColumn.HeaderText = "State";
+            this.stateDataGridViewTextBoxColumn.Name = "stateDataGridViewTextBoxColumn";
+            this.stateDataGridViewTextBoxColumn.ReadOnly = true;
+            this.stateDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // traduccionDataGridViewTextBoxColumn
+            // 
+            this.traduccionDataGridViewTextBoxColumn.DataPropertyName = "Traduccion";
+            this.traduccionDataGridViewTextBoxColumn.HeaderText = "Traduccion";
+            this.traduccionDataGridViewTextBoxColumn.Name = "traduccionDataGridViewTextBoxColumn";
+            this.traduccionDataGridViewTextBoxColumn.ReadOnly = true;
+            this.traduccionDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // verbStateBindingSource
+            // 
+            this.verbStateBindingSource.DataSource = typeof(VerbState);
+            // 
+            // txtFiltro
+            // 
+            this.txtFiltro.Location = new System.Drawing.Point(91, 10);
+            this.txtFiltro.Name = "txtFiltro";
+            this.txtFiltro.Size = new System.Drawing.Size(103, 20);
+            this.txtFiltro.TabIndex = 1;
+            this.txtFiltro.TextChanged += new System.EventHandler(this.txtFiltro_TextChanged);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 315);
+            this.Controls.Add(this.txtFiltro);
+            this.Controls.Add(this.dgvData);
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Versbs State";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verbStateBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.TextBox txtFiltro;
+        private System.Windows.Forms.BindingSource verbStateBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn verbDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn traduccionDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ VerbState/FrmPrincipal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerbState
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            typeof(DataGridView).InvokeMember("DoubleBuffered",
+              BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetProperty, null, dgvData,
+              new object[] { true });
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            CargarInformacion();
+        }
+
+        private void CargarInformacion()
+        {
+            using (dbInglesEntities db = new dbInglesEntities())
+            {
+                var verbs = db.VerbStates.ToList();
+
+                dgvData.DataSource = verbs;
+            }
+        }
+
+        private void txtFiltro_TextChanged(object sender, EventArgs e)
+        {
+            using (dbInglesEntities db = new dbInglesEntities())
+            {
+                var lst = db.VerbStates.Where(x => x.Verb.StartsWith(txtFiltro.Text)).ToList();
+                dgvData.DataSource = lst;
+            }
+        }
+    }
+}
